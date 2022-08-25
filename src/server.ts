4cc4a1a067b1045fcdb20285c0f787ce111dda2c
Alexandro <alexoliveira7x@gmail.com>
2022-08-25 18:24:28 +0100

    refact routing

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specification.routes';
+
 const app = express();
 
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationRoutes);
 
 app.post('/courses', (request, response) => {
   const { name } = request.body;
